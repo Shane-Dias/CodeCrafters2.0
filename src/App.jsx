3ee53f1ed09a56839cfbf7387847d3d177f79873
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-03-15 19:05:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import { RegistrationForm } from "./components/RegistrationForm";
-import { SignUp } from "./components/SignUp";
+// import { RegistrationForm } from "./components/RegistrationForm";
+// import { SignUp } from "./components/SignUp";
 // import Home from "./pages/Home";
 // import About from "./pages/About";
 import Navbar from "./Components/Navbar";
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} /> */}
-        <Route path="/" element={<RegistrationForm />}></Route>
+        {/* <Route path="/" element={<RegistrationForm />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
