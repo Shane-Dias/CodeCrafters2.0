9efed67d3763f3311d8e0bdc422d340caffc71a8
ImpactG1 <145135654+ImpactG1@users.noreply.github.com>
2025-03-15 16:59:45 +0530

    Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< Updated upstream
 
 import Home from "./Pages/Home";
-=======
 import {RegistrationForm} from "./components/RegistrationForm"
 import {SignUp} from "./components/SignUp"
 // import Home from "./pages/Home";
 // import About from "./pages/About";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< Updated upstream
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} /> */}
-=======
        <Route path="/" element={<RegistrationForm />} ></Route>
->>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
